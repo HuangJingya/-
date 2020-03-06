32eb9de5a07ad515bd2c871a086c27c8af161971
Jazlyn <2206283434@qq.com>
2020-03-06 21:50:57 +0800

    基本列表页完成

--- src/js/index.js
+++ src/js/index.js
@@ -27,59 +27,51 @@ var mySwiper = new Swiper('.swiper-container', {
 
 
   //顶部导航栏
-  getList()
+  getRange()
 
-  function getList() {
-    $.ajax({
-      url: '../lib/nav_top.json',
-      dataType: 'json',
-      success: function (res) {
-
-        // 4-1. 准备一个空字符串
-        let str = ''
-        // 4-3. 填充到 nav_mid 里面的 ul 里面
-        $('.nav-mid >ul')
-          .on({
-            mouseenter: () => $('.nav-mid-box').stop().slideDown(),
-            mouseleave: () => $('.nav-mid-box').stop().slideUp()
-          })
-          .children('li') // 找到所有的一级菜单下的 li
-          .on('mouseover', function () {
-            // 5-1. 知道自己移入的时哪一个 li
-            const index = $(this).index()
-            // 5-2. 找到要渲染的数组
-            const list = res[index].list
-            // 5-3. 用我们找到的数组把 nav_box 位置渲染了就可以了
-            let str = ''
-
-            // 5-4. 进行组装
-            list.forEach(item => {
-              str += `
-                <li>
-                  <div>
-                    <img src="${ item.list_url }" alt="">
-                  </div>
-                  <p class="title">${ item.list_name }</p>
-                  <span class="price">${ item.list_price }</span>
-                </li>
-              `
-            })
-
-            // 5-5. 填充到页面里面
-            $('.nav-mid-box > ul').html(str)
-          })
-
-        // 4-4. 给 nav-mid-box 添加一个移入移出事件
-        $('.nav-mid-box')
-          .on({
-            mouseover: function () { $(this).finish().show() },
-            mouseout: function () { $(this).finish().slideUp() }
-          })
-      }
-    })
+  function getRange() {
+      $.ajax({
+          url: '../lib/nav_top.json',
+          dataType: 'json',
+          success: function (res) {
+              console.log(res)
+              let arr=''
+              $('.nav-mid>ul')
+                  .on({
+                      mouseenter: () => $('.nav-mid-box').stop().slideDown(),
+                      mouseleave: () => $('.nav-mid-box').stop().slideUp()
+                  })
+                  .children('li')
+                  .on('mouseover', function () {
+                      const index = $(this).index()
+                      const list = res[index].list
+                      let arr = ''
+  
+                      list.forEach(item => {
+                          arr += ` <li>
+                          <div class="line"><img
+                                  src="${item.list_url}"
+                                  alt=""></div>
+                          <p>${item.list_name}</p>
+                          <span>${item.list_price}</span>
+                      </li> `
+                      })
+                      console.log(arr);
+                      
+                      $('.nav-mid-box>.nav-mid-con>ul').html(arr)
+                  })
+              $('.nav-mid-box')
+                  .on({
+                      mouseover: function () {
+                          $(this).finish().show()},
+                      mouseout: function () {
+                          $(this).finish().slideUp()}
+                  })
+          }
+      })
   }
 
-
+  
 
   //边上的导航栏
   getBannerList()
---
+++ src/js/list.js
@@ -0,0 +1,102 @@
+getList()
+
+function getList() {
+    $.ajax({
+        url: '../lib/goodsList2.json',
+        dataType: 'json',
+        success: function (res) {
+            // 渲染分页器
+            $('.pagi-con').pagination({
+                pageCount: Math.ceil(res.length / 20),
+                current: 1,
+                jump: true,
+                coping: true,
+                homePage: '首页',
+                endPage: '末页',
+                prevContent: '上页',
+                nextContent: '下页',
+                callback: function (api) {
+                    let curr = api.getCurrent();
+                    // console.log(curr);
+
+                    var list = res.slice((curr - 1) * 20, curr * 20)
+                    // console.log(list)
+
+                    //使用分页器渲染一次
+                    bindHtml(list)
+                }
+            })
+            //使用第一页渲染一次
+            bindHtml(res.slice(0, 20))
+        }
+    })
+}
+
+function bindHtml(list) {
+    // console.log(list)
+    let str = ''
+    list.forEach(item => {
+        str += ` <li>
+        <div class="product">
+            <a href=""><img src="${item.image}" alt=""></a>
+        </div>
+        <h6>
+            <a href="">${item.name}</a>
+            <a href="">${item.desc}</a>
+        </h6>
+        <p class="pro-right">
+            <strong>${item.price}</strong>
+            <span>元起</span>
+            <del>${item.del}元</del>
+        </p>
+    </li>`
+    })
+    $('.goods>.goods-con>ul').html(str)
+
+}
+
+
+//顶部导航栏
+getRange()
+
+function getRange() {
+    $.ajax({
+        url: '../lib/nav_top.json',
+        dataType: 'json',
+        success: function (res) {
+            console.log(res)
+            let arr=''
+            $('.nav-mid>ul')
+                .on({
+                    mouseenter: () => $('.nav-mid-box').stop().slideDown(),
+                    mouseleave: () => $('.nav-mid-box').stop().slideUp()
+                })
+                .children('li')
+                .on('mouseover', function () {
+                    const index = $(this).index()
+                    const list = res[index].list
+                    let arr = ''
+
+                    list.forEach(item => {
+                        arr += ` <li>
+                        <div class="line"><img
+                                src="${item.list_url}"
+                                alt=""></div>
+                        <p>${item.list_name}</p>
+                        <span>${item.list_price}</span>
+                    </li> `
+                    })
+                    console.log(arr);
+                    
+                    $('.nav-mid-box>.nav-mid-con>ul').html(arr)
+                })
+            $('.nav-mid-box')
+                .on({
+                    mouseover: function () {
+                        $(this).finish().show()},
+                    mouseout: function () {
+                        $(this).finish().slideUp()}
+                })
+        }
+    })
+}
